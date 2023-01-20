5d87a42d1b62b3b34ecccb26c1a73c6f760e859c
BWBama85 <41128321+BWBama85@users.noreply.github.com>
2023-01-20 00:30:35 -0600

    formatting

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 /**
  * WP Barebones Theme functions and definitions
  *
- * @package WPBarebonesTheme
- * @since WPBarebonesTheme 1.0
+ * @package WP Barebones Theme
+ * @since WP Barebones Theme 1.0
  */
